a255a5b816304d19bee20a2d7c34ee4064746c70
Michael Anderson <Micmail566@gmail.com>
2017-02-07 15:48:58 -0500

    Added a class for Album

--- TheRecordStore/Controllers/HomeController.cs
+++ TheRecordStore/Controllers/HomeController.cs
@@ -12,7 +12,5 @@ namespace TheRecordStore.Controllers
         {
             return View();
         }
-
-       
     }
 }
---
+++ TheRecordStore/Models/Album.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace TheRecordStore.Models
+{
+    public class Album
+    {
+        public int Id { get; set; }
+        public string AlbumName { get; set; }
+        public DateTime AlbumRelease { get; set; }
+        public int BandId { get; set; }
+
+        public virtual Band Band { get; set; }
+    }
+}
